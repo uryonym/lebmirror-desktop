0cff8617fc8a568c9929313d1e939819dc8f4607
uryonet <uryonet@outlook.com>
2021-05-16 07:06:47 +0900

    ページ一覧のデータをgraph apiを利用して取得するように変更

--- src/features/pages/PagesList.tsx
+++ src/features/pages/PagesList.tsx
@@ -1,30 +1,78 @@
-import { Button, ListGroup } from 'react-bootstrap'
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Button, Form, ListGroup, Modal } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectSection } from '../sections/sectionSlice'
+import { createPage, selectPage } from './pageSlice'
 
-const PagesList: React.VFC = () => (
-  <div className="content-list">
-    <Button variant="outline-warning" block className="mb-2">
-      Add a Page
-    </Button>
-    <ListGroup variant="flush">
-      {(() => {
-        const lists = []
-        for (let i = 0; i < 30; i += 1) {
-          lists.push(
-            <ListGroup.Item
-              key={i}
-              action
-              onClick={() => console.log('page link is clicked.')}
-              className="list-item"
-            >
-              page name
-            </ListGroup.Item>
-          )
-        }
-        return lists
-      })()}
-    </ListGroup>
-  </div>
-)
+const PagesList: React.VFC = () => {
+  const dispatch = useDispatch()
+  const { currentSectionId } = useSelector(selectSection)
+  const { pages } = useSelector(selectPage)
+
+  const [show, setShow] = useState(false)
+  const [newPageName, setNewPageName] = useState('')
+
+  const handleClose = () => setShow(false)
+  const handleOpen = () => setShow(true)
+  const handleChangePageName = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPageName(ev.currentTarget.value)
+  }
+  const handleAddPage = () => {
+    if (currentSectionId) {
+      dispatch(createPage(currentSectionId, newPageName))
+      setShow(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="content-list">
+        <Button
+          variant="outline-warning"
+          onClick={handleOpen}
+          block
+          className="mb-2"
+        >
+          Add a Page
+        </Button>
+        <ListGroup variant="flush">
+          {(() => {
+            const lists: any[] = []
+            pages.forEach((page) => {
+              lists.push(
+                <ListGroup.Item key={page.id} action className="list-item">
+                  {page.title}
+                </ListGroup.Item>
+              )
+            })
+            return lists
+          })()}
+        </ListGroup>
+      </div>
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>Add a new page</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control
+            type="text"
+            placeholder="Page Name"
+            onChange={handleChangePageName}
+            value={newPageName}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAddPage}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
 
 export default PagesList
