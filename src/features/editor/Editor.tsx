88917a39c801a244d8d706f9eb4dfbc227ede605
uryonet <uryonet@outlook.com>
2021-05-16 17:08:00 +0900

    エディターにundo, redo機能を実装

--- src/features/editor/Editor.tsx
+++ src/features/editor/Editor.tsx
@@ -3,13 +3,18 @@ import React, { useEffect, useRef } from 'react'
 import { EditorState } from 'prosemirror-state'
 import { schema } from 'prosemirror-schema-basic'
 import { EditorView } from 'prosemirror-view'
+import { history, redo, undo } from 'prosemirror-history'
+import { keymap } from 'prosemirror-keymap'
 
 const Editor: React.VFC = () => {
   const pmEditor = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     if (pmEditor.current) {
-      const state = EditorState.create({ schema })
+      const state = EditorState.create({
+        schema,
+        plugins: [history(), keymap({ 'Mod-z': undo, 'Mod-y': redo })],
+      })
       const view = new EditorView(pmEditor.current, {
         state,
         dispatchTransaction(transaction) {
